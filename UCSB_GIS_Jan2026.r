dc73bf2a0278c833291b579afaafdc01aafa6b70
slkelly22 <108030587+slkelly22@users.noreply.github.com>
2026-01-06 10:57:00 -0600

    watching UCSB's Carpentries Team teach this content before we teach it next week

---
+++ UCSB_GIS_Jan2026.r
@@ -0,0 +1,6 @@
+# UCSB Geospatial Carpentry Online
+# January 6, 8, 9 (skipped Jan 5)
+# Geospatial Data Carpentry for Urbanism using R
+
+# Day 2, January 6, 11:30 - 2:00
+# Intro to Geospatical concepts and vector data
