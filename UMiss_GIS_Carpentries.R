4a18edaa3f5fdd7627f785683ac495e05d589231
slkelly22 <108030587+slkelly22@users.noreply.github.com>
2026-01-14 15:24:37 -0600

    end of Day 1

--- UMiss_GIS_Carpentries.R
+++ UMiss_GIS_Carpentries.R
@@ -1,7 +1,7 @@
 # U of Mississippi GIS Workshop 
 # January 14, 2026 - Day 1
 
-### Harley
+### Harley ### 
 # know where your data file is located (the one you downloaded)
 # anyone have OneDrive...that can create challenges
 # 8:45 Creating a New Project
@@ -158,9 +158,9 @@ head(gapminder_gdp)
 gapminder |> 
   count(continent, country)
 
-## I taught
+## I taught ## 
 
-## Abbie teaching
+### Abbie ### 
 # using Canva slides
 # GIS - Geographic Information System(s)
 # ArcGIS, QGIS (open source), OSM Open Street Maps (open source)
@@ -194,14 +194,87 @@ gapminder |>
 # Raster Data - continuous or grid data using pixals
 # temperature, elevation, gradient
 
+# Attributes are non-spatial data about geographic features
+# geometry without context provides little valuable information
 
+# spatial feature is anything on a map that has a place or shape such as a lake, road, grocery store
+# map layer is a raster or vector dataset added to a map document
+# symbology is the style (color, size, symbols, etc) assigned to vector features based on attribute values
 
+# Shapefiles - the most common geospatial file type, is an open source format for vector data that can be opened in any GIS software
+# Shapefiles are in zip files because they include several different file formats; some of these files are required for the shapefile to work properly
+# common file types include
+# - .shp - feature geometry
+# - .shx - shape index position
+# - .dbf - attribute data
+# - .prj - coordinate system and map project
 
+# Tabular Data - GIS software can read common tabular data formats
+# If there is geographic information included in the data table, GIS tools can be used to transform the table into a shapefile
 
+# Metadata - descriptive information about an object - data about data
 
+# Where to find data
+# Mississippi Geospatial Data Catalog, UnData, etc. 
+# Restricted Access - esri, social explorer, etc. 
 
+# Episode 7 - Open and Plot Vector Layers
 
+library(sf)
+# sf stands for simple features
 
+# import our opening shapefile
+boundary_Delft <- st_read("data/delft-boundary.shp", quiet = TRUE)
+# if you remove quiet = TRUE, then you get the metadata
+boundary_Delft <- st_read("data/delft-boundary.shp")
 
+st_geometry_type(boundary_Delft) # the city of Delft is a polygon
 
+# to see the CRS
+st_crs(boundary_Delft)
 
+st_crs(boundary_Delft)$Name # WGS 84
+st_crs(boundary_Delft)$epsg # 4326
+
+# boundary box
+st_bbox(boundary_Delft) # latitude and longitudinal degrees
+
+# change our CRS for something specifically built for Netherlands and measured in meters
+boundary_Delft <- st_transform(boundary_Delft, crs = 28992)
+st_crs(boundary_Delft)$Name # Amersfoort / RD New
+st_crs(boundary_Delft)$epsg # 28992
+
+# measured in meters? 
+st_bbox(boundary_Delft)
+st_crs(boundary_Delft)$units_gdal # metre; GDAL is a translator R library (out of MIT) for raster and vector files
+
+boundary_Delft # to see everything we just changed
+
+# question from audience: Can you change several shapefiles' CRS at the same time? 
+
+# plot a vector layer
+ggplot(data = boundary_Delft) + 
+  geom_sf(size = 3, color = "black", fill = "cyan1") + 
+  labs(title = "Delft Administrative Boundary") + 
+  coord_sf(datum = st_crs(28992))
+
+# Time is 2:30
+
+# Challenge Activity
+# import shapefiles
+lines_Delft <- st_read("data/delft-streets.shp")
+points_Delft <- st_read("data/delft-leisure.shp")
+
+# what is the crs
+st_crs(lines_Delft) # Amersfoot, 28992
+lines_Delft$geometry # lines
+st_geometry_type(lines_Delft)
+
+st_crs(points_Delft) # Amersfoot, 28992
+points_Delft$geometry # points
+st_geometry_type(points_Delft)
+
+st_bbox(lines_Delft)
+st_bbox(points_Delft)
+
+# Ending at 2:45
